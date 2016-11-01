374c836e31c530224a2f6469a2a7435179a127cb
maxime-beck <mbcompte@gmail.com>
2016-11-01 08:18:22 +0100

    Add class Person

---
+++ src/java/ch/hearc/ig/odi/peoplelist/business/Person.java
@@ -0,0 +1,59 @@
+package ch.hearc.ig.odi.peoplelist.business;
+
+import java.util.Date;
+
+/**
+ * @author maxime.beck
+ */
+public class Person {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Boolean married;
+    private Date birthDate;
+
+    public Person() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Boolean getMarried() {
+        return married;
+    }
+
+    public void setMarried(Boolean married) {
+        this.married = married;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+    
+    
+}
